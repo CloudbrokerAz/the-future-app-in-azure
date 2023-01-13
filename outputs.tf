2ef18d4601e36737dd5c8cddb1afaeb2cc8aa951
Aaron Evans <aarone@outlook.com.au>
2023-01-13 23:11:18 +1100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,7 @@
-# Define your outputs here
+output "futureApp_url" {
+  value = "http://${azurerm_public_ip.futureApp-pip.fqdn}"
+}
+
+output "futureApp_ip" {
+  value = "http://${azurerm_public_ip.futureApp-pip.ip_address}"
+}
